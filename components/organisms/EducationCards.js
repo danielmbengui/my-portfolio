8bb42365e055c9b0d7195c70e4a1bacb6103b589
Daniel Mbengui <couzmec@macbook-air-de-daniel.home>
2023-03-27 00:11:13 +0200

    version with only skills

--- components/organisms/EducationCards.js
+++ components/organisms/EducationCards.js
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 //import ExperienceCard from '../molecules/ExperienceCard';
-import EducationCard from '../molecules/EducationCArd';
+import EducationCard from '../molecules/EducationCard';
 
 //moment().format('MMMM Do YYYY, h:mm:ss a');
 
