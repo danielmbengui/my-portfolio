867ab99c1016cb3a4002376c8fe84158c7bf22e3
danielmbengui <41963926+danielmbengui@users.noreply.github.com>
2025-02-21 23:37:53 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ import Head from 'next/head';
 import Script from 'next/script';
 import { CssBaseline } from '@mui/material';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+//insights
 
 const clientSideEmotionCache = createEmotionCache();
 
