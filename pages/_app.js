7f88c9a76ff0a40afefb17f7d200bfccf66c705d
Daniel Mbengui <daniel.mbengui@gmail.com>
2025-04-01 19:42:53 +0200

    remove xeko from home page

--- pages/_app.js
+++ pages/_app.js
@@ -78,84 +78,6 @@ const MyApp = (props) => {
     //-----------------------------------------------------------------------
   }, []);
 
-  useEffect(() => {
-    function adjustIframeLayout(isOpen) {
-      const widgetIframe = document.getElementById("xeko-ai-widget");
-      if (!widgetIframe) return;
-
-      const isMobile = window.innerWidth < 768;
-
-      if (isMobile) {
-        if (isOpen) {
-          widgetIframe.style.position = "fixed";
-          widgetIframe.style.top = 0;
-          widgetIframe.style.bottom = 0;
-          widgetIframe.style.left = 0;
-          widgetIframe.style.right = 0;
-          widgetIframe.style.width = "100%";
-          widgetIframe.style.height = "100%";
-          //widgetIframe.style.transform = "";
-        } else {
-          widgetIframe.style.position = "fixed";
-          widgetIframe.style.bottom = 0;
-          widgetIframe.style.right = 0;
-          widgetIframe.style.top = "auto";
-          widgetIframe.style.left = "auto";
-          widgetIframe.style.width = "200px";
-          widgetIframe.style.height = "160px";
-          //widgetIframe.style.transform = "";
-        }
-      } else {
-        if (isOpen) {
-          widgetIframe.style.position = "fixed";
-          widgetIframe.style.bottom = 0;
-          widgetIframe.style.right = 0;
-          widgetIframe.style.top = 0;
-          //widgetIframe.style.left = "auto";
-          widgetIframe.style.width = "450px";
-          widgetIframe.style.height = "100%";
-          //widgetIframe.style.transform = "";
-        } else {
-          widgetIframe.style.position = "fixed";
-          //widgetIframe.style.right = "0";
-          widgetIframe.style.top = "auto";
-          //widgetIframe.style.bottom = "0";
-          widgetIframe.style.left = "auto";
-          widgetIframe.style.width = "210px";
-          widgetIframe.style.height = "160px";
-          //widgetIframe.style.transform = "translateY(-50%)";
-
-          widgetIframe.style.right= 0;
-          widgetIframe.style.bottom= 0;
-          //widgetIframe.style.border= '5px solid cyan';
-          //widgetIframe.style.width= '210px';
-          //widgetIframe.style.maxWidth: '250px';
-        }
-      }
-    }
-
-    const handleResize = () => {
-      const currentState = window.xekoWidgetState !== undefined ? window.xekoWidgetState : false;
-      adjustIframeLayout(currentState);
-    };
-
-    const handleMessage = (event) => {
-      if (event.data?.type === "resize_widget") {
-        window.xekoWidgetState = event.data.isOpen;
-        adjustIframeLayout(event.data.isOpen);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("message", handleMessage);
-
-    adjustIframeLayout(false);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("message", handleMessage);
-    };
-  }, []);
 
   return (
     <CacheProvider value={emotionCache}>
@@ -188,17 +110,7 @@ const MyApp = (props) => {
                 <CssBaseline />
                 */
               }
-               <iframe
-                  id="xeko-ai-widget"
-                  src="https://assistant.xeko.ai?assistant_id=67ea9d65dba8839eea322ea3"
-                  style={{
-                    position: 'fixed',
-                    right:0,bottom:0,
-                    //border: '5px solid cyan',
-                    //margin: 0, padding: 0, 
-                    zIndex: 9999
-                  }}
-                  scrolling="no"></iframe>
+              
               <Component {...pageProps} />
               <script type="module" src="https://cdn.jsdelivr.net/npm/ionicons@latest/dist/ionicons/ionicons.esm.js"></script>
 <script noModule src="https://cdn.jsdelivr.net/npm/ionicons@latest/dist/ionicons/ionicons.js"></script>
--- pages/portfolio/skills.js
+++ pages/portfolio/skills.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useWindowSize from '@/hooks/useWindowSize';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { ARRAY_NAMESPACES, ARRAY_LANGAGES } from '@/_mocks_/_settings_items_';
@@ -16,14 +16,102 @@ export default function SkillsPage() {
   const { width } = useWindowSize();
   const { t } = useTranslation();
   const [lang] = useLangMode();
-  const {isMobile} = useDeviceMode();
+  const { isMobile } = useDeviceMode();
 
   const title = t('sections.skills.title');
+  useEffect(() => {
+    function adjustIframeLayout(isOpen) {
+      const widgetIframe = document.getElementById("xeko-ai-widget");
+      if (!widgetIframe) return;
 
+      const isMobile = window.innerWidth < 768;
+
+      if (isMobile) {
+        if (isOpen) {
+          widgetIframe.style.position = "fixed";
+          widgetIframe.style.top = 0;
+          widgetIframe.style.bottom = 0;
+          widgetIframe.style.left = 0;
+          widgetIframe.style.right = 0;
+          widgetIframe.style.width = "100%";
+          widgetIframe.style.height = "100%";
+          //widgetIframe.style.transform = "";
+        } else {
+          widgetIframe.style.position = "fixed";
+          widgetIframe.style.bottom = 0;
+          widgetIframe.style.right = 0;
+          widgetIframe.style.top = "auto";
+          widgetIframe.style.left = "auto";
+          widgetIframe.style.width = "200px";
+          widgetIframe.style.height = "160px";
+          //widgetIframe.style.transform = "";
+        }
+      } else {
+        if (isOpen) {
+          widgetIframe.style.position = "fixed";
+          widgetIframe.style.bottom = 0;
+          widgetIframe.style.right = 0;
+          widgetIframe.style.top = 0;
+          //widgetIframe.style.left = "auto";
+          widgetIframe.style.width = "450px";
+          widgetIframe.style.height = "100%";
+          //widgetIframe.style.transform = "";
+        } else {
+          widgetIframe.style.position = "fixed";
+          //widgetIframe.style.right = "0";
+          widgetIframe.style.top = "auto";
+          //widgetIframe.style.bottom = "0";
+          widgetIframe.style.left = "auto";
+          widgetIframe.style.width = "210px";
+          widgetIframe.style.height = "160px";
+          //widgetIframe.style.transform = "translateY(-50%)";
+
+          widgetIframe.style.right = 0;
+          widgetIframe.style.bottom = 0;
+          //widgetIframe.style.border= '5px solid cyan';
+          //widgetIframe.style.width= '210px';
+          //widgetIframe.style.maxWidth: '250px';
+        }
+      }
+    }
+
+    const handleResize = () => {
+      const currentState = window.xekoWidgetState !== undefined ? window.xekoWidgetState : false;
+      adjustIframeLayout(currentState);
+    };
+
+    const handleMessage = (event) => {
+      if (event.data?.type === "resize_widget") {
+        window.xekoWidgetState = event.data.isOpen;
+        adjustIframeLayout(event.data.isOpen);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("message", handleMessage);
+
+    adjustIframeLayout(false);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
   return (
     <>
       {!isMobile ? <AppBarComponent title={title} /> : <MobileAppBarComponent title={title} />}
       {!isMobile ? <ContentComponent><SkillsComponent /></ContentComponent> : <MobileContentComponent><MobileSkillsComponent /></MobileContentComponent>}
+      <iframe
+        id="xeko-ai-widget"
+        src="https://assistant.xeko.ai?assistant_id=67ea9d65dba8839eea322ea3"
+        style={{
+          position: 'fixed',
+          right: 0, bottom: 0,
+          //border: '5px solid cyan',
+          //margin: 0, padding: 0, 
+          zIndex: 9999
+        }}
+        scrolling="no"></iframe>
     </>
   );
 }
